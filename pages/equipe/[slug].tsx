d85be7f727a5c6e305aea7057db4b94e69b8ac0a
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2020-11-06 23:38:53 +0000

    Fixes

--- pages/equipe/[slug].tsx
+++ pages/equipe/[slug].tsx
@@ -1,4 +1,9 @@
-export default function Equipe({}) {
+import Image from "next/image";
+import { fetchAPI } from "../../lib/api";
+import markdownToHtml from "../../lib/markdownToHtml";
+import markdownStyles from "../../components/markdown-styles.module.css";
+
+export default function Equipe({ equipe }) {
   return (
     <div className="relative py-16 bg-white overflow-hidden">
       <div className="absolute top-2 right-2">
@@ -18,9 +23,9 @@ export default function Equipe({}) {
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
@@ -128,94 +133,83 @@ export default function Equipe({}) {
       </div> */}
       <div className="relative px-4 sm:px-6 lg:px-8">
         <div className="text-lg max-w-prose mx-auto mb-6">
-          <p className="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
+          <p className="text-base text-center leading-6 text-blue-600 font-semibold tracking-wide uppercase">
             Une équipe
           </p>
-          <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-            JavaScript for Beginners
+          <h1 className="mt-2 mb-2 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+            {equipe?.nom}
           </h1>
+          <p className="text-center mb-8 text-xl text-gray-500 leading-8">
+            {equipe?.titre}
+          </p>
+        </div>
+
+        <div className="prose prose-lg text-gray-500 mx-auto">
           <figure>
-            <img
-              className="w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-              alt=""
+            <Image
+              className="w-full rounded-lg object-contain"
+              src={equipe?.photo.url}
+              alt={equipe?.nom}
               width="1310"
               height="873"
             />
-            <figcaption>
-              Sagittis scelerisque nulla cursus in enim consectetur quam.
-            </figcaption>
           </figure>
-          <p className="text-xl text-gray-500 leading-8">
-            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
-            arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
-            feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-            Eleifend egestas fringilla sapien.
-          </p>
-        </div>
-        <div className="prose prose-lg text-gray-500 mx-auto">
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{" "}
-            sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet
-            vitae sed turpis id. Id dolor praesent donec est. Odio penatibus
-            risus viverra tellus varius sit neque erat velit. Faucibus commodo
-            massa rhoncus, volutpat. Dignissim sed eget risus enim.{" "}
-            <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
-          </p>
-          <ul>
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>
-              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-            </li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
-          <p>
-            Quis semper vulputate aliquam venenatis egestas sagittis quisque
-            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
-            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
-            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
-            elit interdum dignissim.
-          </p>
-          <h2>From beginner to expert in 30 days</h2>
-          <p>
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-            diam.
-          </p>
-          <blockquote>
-            <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
-            </p>
-          </blockquote>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
 
-          <h3>Everything you need to get up and running</h3>
-          <p>
-            Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>.
-            Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-            erat ac. Cras fermentum convallis quam.
-          </p>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
+          {/* <div className="max-w-2xl mx-auto"> */}
+          <div
+            className={markdownStyles["markdown"]}
+            dangerouslySetInnerHTML={{ __html: equipe?.presentation }}
+          />
         </div>
       </div>
     </div>
   );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await fetchAPI(
+    `query EquipeBySlug($slug: String) {
+  equipe(filter: {slug: {eq: $slug}}) {
+    nom
+    slug
+    photo {
+      url
+    }
+    titre
+    presentation
+  }
+}`,
+    {
+      preview,
+      variables: {
+        slug: params.slug,
+      },
+    }
+  );
+
+  const presentation = await markdownToHtml(data?.equipe?.presentation || "");
+
+  return {
+    props: {
+      preview,
+      equipe: {
+        ...data?.equipe,
+        presentation,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await fetchAPI(`query AllEquipe {
+    allEquipes(orderBy:nom_ASC,first:20){
+      slug
+    }
+  }
+  `);
+
+  return {
+    paths: data?.allEquipes?.map((equipe) => `/equipe/${equipe?.slug}`) || [],
+    fallback: false,
+  };
 }
